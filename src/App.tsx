d6c7e6bad1ab8862c69f68cbbe4d0838b8767e50
juanfeoru <felipeortega383@gmail.com>
2026-09-16 09:58:50 -0500

    feat: create home hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import Header from "./components/layout/Header";
+import Home from "./pages/Home";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+
+      <main>
+        <Home />
+      </main>
+    </>
+  );
 }
 
 export default App;
