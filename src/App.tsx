ddacb42339403fc17bc8b88dc75d4e3febdbcdd5
juanfeoru <felipeortega383@gmail.com>
2026-09-16 12:27:00 -0500

    feat: create favorites page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import Header from "./components/layout/Header";
 import Home from "./pages/Home";
 import type { Movie } from "./data/movies";
+import Favorites from "./pages/Favorites";
+import { Route, Routes } from "react-router";
 
 function App() {
   const [favorites, setFavorites] = useState<Movie[]>([]);
@@ -25,7 +27,24 @@ function App() {
       <Header />
 
       <main>
-        <Home handleFavorites={handleFavorites} favorites={favorites} />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home handleFavorites={handleFavorites} favorites={favorites} />
+            }
+          />
+
+          <Route
+            path="/favorites"
+            element={
+              <Favorites
+                favorites={favorites}
+                handleFavorites={handleFavorites}
+              />
+            }
+          />
+        </Routes>
       </main>
     </>
   );
