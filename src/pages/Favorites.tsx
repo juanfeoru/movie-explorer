42b371d4b25ed895dba2fcd120e02bf22426574b
juanfeoru <felipeortega383@gmail.com>
2026-09-21 17:48:15 -0500

    style: change grid favorites

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -54,7 +54,7 @@ export default function Favorites({
           </p>{" "}
         </div>
 
-        <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
           {favorites.map((favorite) => (
             <MovieCard
               key={favorite.id}
